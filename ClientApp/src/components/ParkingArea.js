f33351d32f915c398ccb09663893490677d6f376
7niako99 <127883734+7niako99@users.noreply.github.com>
2023-04-27 23:00:14 +0300

    add new files

---
+++ ClientApp/src/components/ParkingArea.js
@@ -0,0 +1,54 @@
+<div class="row">
+    <div class="large-12 columns">
+        <h1>Parking ares</h1>
+        <p><a href="#" ><i class="fa fa-plus-square"></i>Add Parking area</a></p>
+        <table id="test-table">
+            <thead>
+                <tr>
+                    <th>Id</th>
+                    <th>Parking area Name</th>
+                    <th>Hours opening</th>
+                    <th>Adress</th>
+                    <th>Longtitude</th>
+                    <th>Latitude</th>
+                </tr>
+            </thead>
+            <tfoot></tfoot>
+            <tbody>
+                <tr>
+                    <td><label for=""><input type="text" name="" id="" placeholder="Id" /></label></td>
+                    <td><label for=""><input type="text" name="" id="" placeholder="name" /></label></td>
+                    <td><label for=""><input type="text" name="" id="" placeholder="hoursOpening" /></label></td>
+                    <td><label for=""><input type="text" name="" id="" placeholder="adress" /></label></td>
+                </tr>
+
+                <tr>
+                    <td>1</td>
+                    <td>Best parking area 1</td>
+                    <td>08:00-21:00</td>
+                    <td>Maizonos str1</td>
+                    <td>
+                        <form action="">
+                            <button class="button"><i class="fa fa-pencil-square-o"></i></button>
+                            <button class="button"><i class="fa fa-trash-o"></i></button>
+                        </form>
+                    </td>
+                </tr>
+                <tr>
+                    <td>2</td>
+                    <td>Best parking area 2</td>
+                    <td>08:00-21:00</td>
+                    <td>Maizonos str23</td>
+                    <td>
+                        <form action="">
+                            <button class="button"><i class="fa fa-pencil-square-o"></i></button>
+                            <button class="button"><i class="fa fa-trash-o"></i></button>
+                        </form>
+                    </td>
+                </tr>
+
+            </tbody>
+        </table>
+        <button class="tiny button right">Add Parking area</button>
+    </div>
+</div>
